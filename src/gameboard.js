078a8339d3fc878be210fbdce351f14b246a1e1a
Sebastien <sebastienpj@protonmail.com>
2022-06-05 10:17:01 -0500

    gameBoard check attack received is a hit +  tests

--- src/gameboard.js
+++ src/gameboard.js
@@ -1,11 +1,32 @@
 function gameboardFactory() {
+  const missedAttacks = [];
+
   function setShip(ship, coordinates) {
     ship.setCoordinates(coordinates);
   }
 
-  function receiveAttack(coordinate) {
-    const row = coordinate[0];
-    const col = coordinate[1];
+  // determines whether or not the attack hit a ship
+  // and then sends the ‘hit’ function to the correct ship,
+  // or records the coordinates of the missed shot.
+
+  function attackHitShip(attackCoordinate, ship) {
+    // check if coord is in ship.getCoordinates [[0, 3], [1, 3], [2, 3]]
+    const shipCoordinatesJSON = JSON.stringify(ship.getCoordinates());
+
+    return shipCoordinatesJSON.includes(JSON.stringify(attackCoordinate));
+  }
+
+  function receiveAttack(coordinate, allShips) {
+    // checks if the coordinate of attack matches any of the coordinates
+    // where ships are placed
+
+    allShips.forEach((ship) => {
+      if (attackHitShip(coordinate, ship)) {
+        ship.isHit();
+      } else {
+        missedAttacks.push(coordinate);
+      }
+    });
   }
 
   return { receiveAttack, setShip };
--- src/gameboard.test.js
+++ src/gameboard.test.js
@@ -1,10 +1,39 @@
 import { gameboardFactory } from './gameboard';
 
-test('Gameboard calls ship factory to set ship', () => {
+test('Calls shipFactory to set ship', () => {
   const myShip = {
     setCoordinates: jest.fn(),
   };
   const myGameboard = gameboardFactory();
   myGameboard.setShip(myShip, null);
   expect(myShip.setCoordinates).toHaveBeenCalled();
+});
+
+test('Sends call to ship if hit', () => {
+  const ship1 = {
+    getCoordinates: () => JSON.stringify([[0, 3], [1, 3], [2, 3]]),
+    isHit: jest.fn(),
+  };
+
+  const allShipsArray = [ship1];
+  const gameBoard = gameboardFactory();
+  gameBoard.receiveAttack(JSON.stringify([0, 3]), allShipsArray);
+  expect(ship1.isHit).toHaveBeenCalled();
+});
+
+test('Does not call ship if not hit', () => {
+  const ship1 = {
+    getCoordinates: () => JSON.stringify([[0, 3], [1, 3], [2, 3]]),
+    isHit: jest.fn(),
+  };
+
+  const ship2 = {
+    getCoordinates: () => JSON.stringify([[1, 0], [1, 1]]),
+    isHit: jest.fn(),
+  };
+
+  const allShipsArray = [ship1, ship2];
+  const gameBoard = gameboardFactory();
+  gameBoard.receiveAttack(JSON.stringify([0, 3]), allShipsArray);
+  expect(ship2.isHit).not.toHaveBeenCalled();
 });
