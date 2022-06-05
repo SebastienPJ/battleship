3f080b3ca3df4f4fcd23e84f60830b140d02f128
Sebastien <sebastienpj@protonmail.com>
2022-06-04 19:44:09 -0500

    simplify gameboard setShip test

--- src/gameboard.test.js
+++ src/gameboard.test.js
@@ -3,10 +3,8 @@ import { gameboardFactory } from './gameboard';
 test('Gameboard calls ship factory to set ship', () => {
   const myShip = {
     setCoordinates: jest.fn(),
-
   };
-  const coords = [[0, 3], [1, 3], [2, 3]];
   const myGameboard = gameboardFactory();
-  myGameboard.setShip(myShip, coords);
+  myGameboard.setShip(myShip, null);
   expect(myShip.setCoordinates).toHaveBeenCalled();
 });
